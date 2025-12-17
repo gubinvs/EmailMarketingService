f6227d3dad72bcb534e058c350feb29a5fa63c3a
master <gubinvs@gmail.com>
2025-12-17 21:09:02 +0300

    sd

--- obj/Debug/net9.0/EmailMarketingService.AssemblyInfo.cs
+++ obj/Debug/net9.0/EmailMarketingService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmailMarketingService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c51115b2f9a5e35b2ebced8c44c59aa0909e8dd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf0888bed0cf5b357a96dc37956d6d8875638349")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmailMarketingService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmailMarketingService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
